ba87e51ffc82af8fae3d5464f5b5d32898dfbf77
Hamzeh Khanpour <hamzeh.khanpour@gmail.com>
2023-11-22 18:56:17 +0100

    slepton

---
+++ yy_Corrected_with_MN2_mMin2_q2min_26July/trapint_DM_Hamzeh.py
@@ -0,0 +1,177 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import math
+
+
+plt.rcParams["axes.linewidth"] = 1.8
+plt.rcParams["xtick.major.width"] = 1.8
+plt.rcParams["xtick.minor.width"] = 1.8
+plt.rcParams["ytick.major.width"] = 1.8
+plt.rcParams["ytick.minor.width"] = 1.8
+
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+
+plt.rcParams["xtick.labelsize"] = 15
+plt.rcParams["ytick.labelsize"] = 15
+
+plt.rcParams["legend.fontsize"] = 15
+
+plt.rcParams['legend.title_fontsize'] = 'x-large'
+
+
+
+
+
+def cs_higgs_w(wvalue):
+    re = 2.8179403262e-15 * 137.0 / 128.0
+    me = 0.510998950e-3
+    mDM = 10.0
+    hbarc2 =  0.389
+    alpha2 = (1.0/137.0)*(1.0/137.0)
+    # alpha2 = alpha * alpha
+
+    if (1.0 - 4.0 * mDM * mDM / wvalue) >= 0:
+        beta = np.sqrt(1.0 - 4.0 * mDM * mDM / wvalue)
+    else:
+        # Handle the case where the expression is negative (e.g., set beta to NaN)
+        beta = np.nan
+
+    if wvalue > mDM:
+        cs = (4.0 * np.pi * alpha2 * hbarc2 ) / wvalue* (beta) * \
+             (2.0 - beta**2.0 - (1-beta**4.0)/(2.0 * beta)*math.log((1.0+beta)/(1.0-beta)))
+    else:
+        cs = 0.
+
+    return cs
+    
+
+def trap_integ(wv, fluxv):
+    wmin = np.zeros(len(wv) - 1)
+    integ = np.zeros(len(wv) - 1)
+
+    for i in range(len(wv) - 2, -1, -1):
+        wvwid = wv[i + 1] - wv[i]
+        cs_0 = cs_higgs_w(wv[i])
+        cs_1 = cs_higgs_w(wv[i + 1])
+        traparea = wvwid * 0.5 * (fluxv[i] * cs_0 + fluxv[i + 1] * cs_1)
+        wmin[i] = wv[i]
+        if i == len(wv) - 2:
+            integ[i] = traparea
+        else:
+            integ[i] = integ[i + 1] + traparea
+
+    nanobarn = 1.e+40
+
+    return wmin, integ * 1000000000.0
+
+
+sys.path.append('./values')
+
+from wgrid_1_4_4_0908 import *
+
+wv = np.array(wvalues[3])
+ie = np.array(inel[3])
+el = np.array(elas[3])
+
+wv1, int_inel = trap_integ(wv, ie)
+wv2, int_el = trap_integ(wv, el)
+
+fig, ax = plt.subplots(figsize = (9., 8.))
+ax.set_xlim(100., 1000.)
+ax.set_ylim(1.e-9, 1.e4)
+
+
+inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}$ GeV$^2$)').format(inel[2])
+title_label = ('$Q^2_e<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$').format(10,np.log10(inel[1]))
+plt.loglog(wv2[:101], int_el[:101], linestyle = 'solid',  linewidth=2,  label = 'elastic')
+plt.loglog(wv1[:101], int_inel[:101], linestyle = 'dotted',  linewidth=2, label = inel_label)
+
+#plt.grid()
+
+plt.legend(title = title_label)
+
+#plt.grid()
+
+
+
+
+
+from wgrid_2_4_4_0908 import *
+
+wv = np.array(wvalues[3])
+ie = np.array(inel[3])
+wv1, int_inel = trap_integ(wv, ie)
+
+inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$)').format(10,np.log10(inel[2]))
+plt.loglog(wv2[:101], int_inel[:101], linestyle = 'dashdot',  linewidth=2, label = inel_label)
+plt.legend(title = title_label)
+
+
+
+
+
+
+
+
+
+
+from wgrid_3_4_4_0908 import *
+
+wv = np.array(wvalues[3])
+ie = np.array(inel[3])
+wv1, int_inel = trap_integ(wv, ie)
+
+inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$)').format(10,np.log10(inel[2]))
+plt.loglog(wv2[:101], int_inel[:101], linestyle = 'dashdot',  linewidth=2, label = inel_label)
+plt.legend(title = title_label)
+
+
+
+
+
+
+
+font1 = {'family':'serif','color':'black','size':24}
+font2 = {'family':'serif','color':'black','size':24}
+
+plt.xlabel("W$_0$ [GeV]",  fontdict = font2)
+plt.ylabel("$\sigma_{S^+S^-}$ (W > W$_0$) [pb]", fontdict = font2)
+
+
+
+plt.savefig("cs_SpSm_MN2_mMin2_q2min_Final.pdf")
+plt.savefig("cs_SpSm_MN2_mMin2_q2min_Final.jpg")
+
+
+
+plt.show()
+
+
+
+
+
+"""
+from wgrid_2_4_4_0908 import *
+
+wv = np.array(wvalues[3])
+ie = np.array(inel[3])
+wv1, int_inel = trap_integ(wv, ie)
+
+inel_label = 'M_N < ' + str(inel[0])
+plt.loglog(wv1[:101], int_inel[:101], '-', label = inel_label)
+plt.legend(title = title_label)
+
+from wgrid_3_4_4_0908 import *
+
+wv = np.array(wvalues[3])
+ie = np.array(inel[3])
+wv1, int_inel = trap_integ(wv, ie)
+
+inel_label = 'M_N < ' + str(inel[0])
+plt.loglog(wv2[:101], int_inel[:101], '-', label = inel_label)
+plt.legend(title = title_label)
+"""
+
+
