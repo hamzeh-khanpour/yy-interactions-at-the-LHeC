d1bde5000d40bea415d3f0b93614a511c6f24772
Hamzeh Khanpour <hamzeh.khanpour@gmail.com>
2024-04-17 20:06:12 +0200

    comparisons of EPA with cepgen
    comparisons of EPA with cepgen for yy @ LHeC project

--- syy_Corrected_with_MN2_mMin2_q2min_26July/syyscanMN-10-100000-100000.py
+++ syy_Corrected_with_MN2_mMin2_q2min_26July/syyscanMN-10-100000-100000.py
@@ -30,7 +30,7 @@ inelastic = True
 for wv in wlist:
     print('w, nMmax, q2emax, q2pmax:', wv, mNmax, q2emax, q2pmax)
     if inelastic:
-        flux_inel_w = Syy.flux_inel_yy_atW(wv, 50., 7000., q2emax, mNmax, q2pmax)
+        flux_inel_w = Syy.flux_inel_yy_atW(wv, 50.0, 7000.0, q2emax, mNmax, q2pmax)
         # print(flux_inel_w)
         syy = 2.0 * flux_inel_w[0] / wv
         res_inel[3].append(syy)
@@ -38,7 +38,7 @@ for wv in wlist:
               .format(wv, syy, flux_inel_w[0], flux_inel_w[1]))
 
 
-    flux_el = Syy.flux_el_yy_atW(wv, 50., 7000., q2emax, q2pmax)
+    flux_el = Syy.flux_el_yy_atW(wv, 50.0, 7000.0, q2emax, q2pmax)
     # print(flux_el)
     syy = 2.0 * flux_el[0] / wv
     res_el[3].append(syy)
--- syy_Corrected_with_MN2_mMin2_q2min_26July/syyscanMN-3-1-1.py
+++ syy_Corrected_with_MN2_mMin2_q2min_26July/syyscanMN-3-1-1.py
@@ -8,8 +8,8 @@ import numpy as np
 # last two arguments: mNmax (not squared), Q2max (squared)
 
 mNmax = 3.0
-q2emax = 10.0
-q2pmax = 0.5
+q2emax = 1.0
+q2pmax = 1.0
 # wlist = [10., 20., 50., 100., 200., 500.]
 # wlist = [200.]
 # wln = np.linspace(1., 3., 41)
@@ -30,7 +30,7 @@ inelastic = True
 for wv in wlist:
     print('w, nMmax, q2emax, q2pmax:', wv, mNmax, q2emax, q2pmax)
     if inelastic:
-        flux_inel_w = Syy.flux_inel_yy_atW(wv, 50., 7000., q2emax, mNmax, q2pmax)
+        flux_inel_w = Syy.flux_inel_yy_atW(wv, 50.0, 7000.0, q2emax, mNmax, q2pmax)
         # print(flux_inel_w)
         syy = 2.0 * flux_inel_w[0] / wv
         res_inel[3].append(syy)
@@ -38,7 +38,7 @@ for wv in wlist:
               .format(wv, syy, flux_inel_w[0], flux_inel_w[1]))
 
 
-    flux_el = Syy.flux_el_yy_atW(wv, 50., 7000., q2emax, q2pmax)
+    flux_el = Syy.flux_el_yy_atW(wv, 50.0, 7000.0, q2emax, q2pmax)
     # print(flux_el)
     syy = 2.0 * flux_el[0] / wv
     res_el[3].append(syy)
@@ -47,7 +47,7 @@ for wv in wlist:
 
 
 
-with open('3_10_05.dat', 'w') as f:
+with open('3_1_1.dat', 'w') as f:
     print(res_param, file = f)     
     print(res_inel, file = f)
     print(res_el, file = f)
