aef19604ae9adefd59b51e24e133e9166d95a7bf
Hamzeh Khanpour <58598278+hamzeh-khanpour@users.noreply.github.com>
2024-05-07 13:29:20 +0200

    update for Higgs
    update for Higgs

--- yy_Corrected_with_MN2_mMin2_q2min_20April_ALLM/trapint_Higgs_Hamzeh.py
+++ yy_Corrected_with_MN2_mMin2_q2min_20April_ALLM/trapint_Higgs_Hamzeh.py
@@ -20,71 +20,36 @@ plt.rcParams["legend.fontsize"] = 15
 plt.rcParams['legend.title_fontsize'] = 'x-large'
 
 
-####################################################################
-
-def cs_higgs_w_condition(wvalue):
-    re = 2.8179403262e-15 * 137.0 / 128.0
-    me = 0.510998950e-3
-    MH = 125.0
-    G = 4.2e-3
-    Gyy = (2.27e-3)*(4.2e-3)
-    hbarc2 =  0.389
-    alpha2 = (1.0/137.0)*(1.0/137.0)
-
-    # Use np.greater for element-wise comparison
-    condition = np.greater(wvalue, MH)
-    cs = np.where(condition, (8.0 * np.pi * np.pi * hbarc2) * (Gyy / MH) * (1.0 / np.pi) *
-                  ((MH * G) / ((MH * MH - wvalue * wvalue)*(MH * MH - wvalue * wvalue) + MH * MH * G * G)) * 1e9, 0.0)
-
-    return cs
-
-####################################################################
 
 def cs_higgs_w(wvalue):
     re = 2.8179403262e-15 * 137.0 / 128.0
     me = 0.510998950e-3
     MH = 125.0
-    G = 4.2e-3
+    G  = 4.2e-3
     Gyy = (2.27e-3)*(4.2e-3)
     hbarc2 =  0.389
     alpha2 = (1.0/137.0)*(1.0/137.0)
+#    alpha2 = alpha * alpha
 
-    cs = (8.0 * np.pi * np.pi* hbarc2 ) * (Gyy / MH)* (1.0/ np.pi) * \
-         ( (MH *G)/((MH*MH - wvalue*wvalue)*(MH*MH-wvalue*wvalue) + MH*MH*G*G)) * 1e9
 
-    return cs
 
-####################################################################
-
-# Function to calculate cross-section
-def higgs_cross_section_final(wvalue):
-
-    # Constants
-    M_H = 125.0  # Higgs mass in GeV
-    Gamma = 4.07e-3  # Total width
-    Gamma_gamma = (2.27e-3)*(Gamma)  # Two-photon width
-    hbarc2 =  0.389
-
-    cs = 4.0 * np.pi ** 2.0 * Gamma_gamma * hbarc2 / M_H**2.0  * 1e9
+    if wvalue > MH:
+        cs = (8.0 * np.pi * np.pi* hbarc2 ) * (Gyy / MH)* (1./ np.pi) * \
+         ( (MH *G)/((MH*MH - wvalue*wvalue)*(MH*MH-wvalue*wvalue) + MH*MH*G*G))
+    else:
+        cs = 0.0
 
     return cs
 
 
-####################################################################
-
-
-
-    
 def trap_integ(wv, fluxv):
     wmin = np.zeros(len(wv) - 1)
     integ = np.zeros(len(wv) - 1)
 
     for i in range(len(wv) - 2, -1, -1):
         wvwid = wv[i + 1] - wv[i]
-        cs_0 = higgs_cross_section_final(wv[i])
-        cs_1 = higgs_cross_section_final(wv[i + 1])
-#        cs_0 = cs_higgs_w(wv[i])
-#        cs_1 = cs_higgs_w(wv[i + 1])
+        cs_0 = cs_higgs_w(wv[i])
+        cs_1 = cs_higgs_w(wv[i + 1])
         traparea = wvwid * 0.5 * (fluxv[i] * cs_0 + fluxv[i + 1] * cs_1)
         wmin[i] = wv[i]
         if i == len(wv) - 2:
@@ -94,11 +59,10 @@ def trap_integ(wv, fluxv):
 
     nanobarn = 1.e+40
 
-    return wmin, integ  # * 1000000000.0
+    return wmin, integ * 1000000000
 
 
 sys.path.append('./values')
-
 
 
 
@@ -111,18 +75,23 @@ el = np.array(elas[3])
 wv1, int_inel = trap_integ(wv, ie)
 wv2, int_el = trap_integ(wv, el)
 
-fig, ax = plt.subplots(figsize = (9.0, 8.0))
+fig, ax = plt.subplots(figsize = (9., 8.))
 ax.set_xlim(100.0, 1000.0)
-ax.set_ylim(1.e-8, 1.e1)
+ax.set_ylim(1.e-15, 1.e-4)
+
+
+
 
 
 inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}$ GeV$^2$)').format(inel[2])
 title_label = ('$Q^2_e<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$').format(10,np.log10(inel[1]))
-plt.loglog(wv2[:303], int_el[:303], linestyle = 'solid',  linewidth=2,  label = 'elastic')
+plt.loglog(wv2[:303], int_el[:303], linestyle = 'solid',  linewidth=2,  label = 'tagged elastic')
 plt.loglog(wv1[:303], int_inel[:303], linestyle = 'dotted',  linewidth=2, label = inel_label)
 
 #plt.grid()
 
+
+
 plt.legend(title = title_label)
 
 #plt.grid()
@@ -131,40 +100,24 @@ plt.legend(title = title_label)
 
 
 
-from wgrid_50_100000_1000 import *
+"""
+from wgrid_3_4_4_0908 import *
 
 wv = np.array(wvalues[3])
 ie = np.array(inel[3])
 wv1, int_inel = trap_integ(wv, ie)
 
 inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$)').format(10,np.log10(inel[2]))
-plt.loglog(wv2[:303], int_inel[:303], linestyle = 'dashdot',  linewidth=2, label = inel_label)
+plt.loglog(wv2[:101], int_inel[:101], linestyle = 'dashdot',  linewidth=2, label = inel_label)
 plt.legend(title = title_label)
-
-
-
-
-
-
-from wgrid_300_100000_100000 import *
-
-wv = np.array(wvalues[3])
-ie = np.array(inel[3])
-wv1, int_inel = trap_integ(wv, ie)
-
-inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$)').format(10,np.log10(inel[2]))
-plt.loglog(wv2[:303], int_inel[:303], linestyle = 'dashdot',  linewidth=2, label = inel_label)
-plt.legend(title = title_label)
-
-
+"""
 
 
 
 # Save the output values in a text file
-output_data = np.column_stack((wv2[:303], int_el[:303], int_inel[:303]))
+output_data = np.column_stack((wv2[:101], int_el[:101], int_inel[:101]))
 header = 'W_Value Elastic Inelastic'
 np.savetxt('output_values_Higgs.txt', output_data, header=header, fmt='%0.8e', delimiter='\t')
-
 
 
 
@@ -176,17 +129,13 @@ plt.xlabel("W$_0$ [GeV]",  fontdict = font2)
 plt.ylabel("$\sigma_{higgs}$ (W > W$_0$) [pb]", fontdict = font2)
 
 
+plt.savefig("cs_higgs_MN2_mMin2_q2min_Final_20April.pdf")
+plt.savefig("cs_higgs_MN2_mMin2_q2min_Final_20April.jpg")
 
-plt.savefig("cs_Higgs_MN2_mMin2_q2min_Final_20April.pdf")
-plt.savefig("cs_Higgs_MN2_mMin2_q2min_Final_20April.jpg")
 
 
 
 plt.show()
-
-
-
-
 
 """
 from wgrid_2_4_4_0908 import *
@@ -196,7 +145,7 @@ ie = np.array(inel[3])
 wv1, int_inel = trap_integ(wv, ie)
 
 inel_label = 'M_N < ' + str(inel[0])
-plt.loglog(wv1[:202], int_inel[:202], '-', label = inel_label)
+plt.loglog(wv1[:101], int_inel[:101], '-', label = inel_label)
 plt.legend(title = title_label)
 
 from wgrid_3_4_4_0908 import *
@@ -206,7 +155,7 @@ ie = np.array(inel[3])
 wv1, int_inel = trap_integ(wv, ie)
 
 inel_label = 'M_N < ' + str(inel[0])
-plt.loglog(wv2[:202], int_inel[:202], '-', label = inel_label)
+plt.loglog(wv2[:101], int_inel[:101], '-', label = inel_label)
 plt.legend(title = title_label)
 """
 
