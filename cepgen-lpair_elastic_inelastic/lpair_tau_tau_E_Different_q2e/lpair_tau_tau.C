2ca5f68f280f23786a1876c88d9b07b3267de524
Hamzeh Khanpour <hamzeh.khanpour@gmail.com>
2024-05-31 14:45:24 +0200

    plots

--- cepgen-lpair_elastic_inelastic/lpair_tau_tau_E_Different_q2e/lpair_tau_tau.C
+++ cepgen-lpair_elastic_inelastic/lpair_tau_tau_E_Different_q2e/lpair_tau_tau.C
@@ -18,7 +18,7 @@ TFile *F;
     TH1 *histMassdilepton =  new TH1F("M_{inv}", "", 500, 0.0, 500.0);
     TH1 *histPtdilepton   =  new TH1F("Pt", "",      50, 0.0, 10.0);
 
-    TH1 *histq2           =  new TH1F("q2", "",      100, 0.0, 100000.0);
+    TH1 *histq2           =  new TH1F("q2", "",      1000, 0.0, 1000.0);
 
     TH1 *histq2prime      =  new TH1F("q2prime", "", 50, 0.0, 100.0);
 
@@ -44,6 +44,9 @@ TFile *F;
 
 
 
+    Float_t bin_width_correction = 1000.0/1000.0;
+
+
     Float_t Mll = 0.0;
     Float_t Ptll = 0.0;
     Float_t Q2 = 0.0;
@@ -60,6 +63,7 @@ TFile *F;
 
 
    Float_t  integrated_luminosity = 0.0;
+
    Float_t  integrated_cross_section_value_BH = 0.0;
 
    Float_t  event_weight_BH  = 0.0;
@@ -121,16 +125,13 @@ void lpair_tau_tau::Loop()
       // if (Cut(ientry) < 0) continue;
 
 
-
-
-
       Float_t  integrated_luminosity = 1.0; // fb^{-1}
 
 //      Float_t  integrated_cross_section_value_BH  = 4.55272144e+01;   //   pb  q2 = 10 GeV^2
 //      Float_t  integrated_cross_section_value_BH  = 4.88223395e+01;   //   pb  q2 = 100 GeV^2
-//      Float_t  integrated_cross_section_value_BH  = 4.96239207e+01;   //   pb  q2 = 1000 GeV^2
+      Float_t  integrated_cross_section_value_BH  = 4.96239207e+01;   //   pb  q2 = 1000 GeV^2
 //      Float_t  integrated_cross_section_value_BH  = 4.95740596e+01;   //   pb  q2 = 10000 GeV^2
-      Float_t  integrated_cross_section_value_BH  = 4.96574771e+01;   //   pb  q2 = 100000 GeV^2
+//      Float_t  integrated_cross_section_value_BH  = 4.96574771e+01;   //   pb  q2 = 100000 GeV^2
 
       Float_t  event_weight_BH  = integrated_cross_section_value_BH  * integrated_luminosity / nentries;
 
@@ -262,7 +263,9 @@ void lpair_tau_tau::Loop()
       histMassdilepton->Fill(Mll,event_weight_BH);
       histPtdilepton->Fill(Ptll,event_weight_BH);
 
-      histq2->Fill(q2,event_weight_BH);
+
+      histq2->Fill(q2, event_weight_BH * bin_width_correction);
+
 
       histq2prime->Fill(q2prime);
 
@@ -305,7 +308,7 @@ TLatex *t2a = new TLatex(0.5,0.9,"#bf{LHeC}");
                 t2a->SetTextAlign(20);
 
 
-TLatex *t3a = new TLatex(0.40,0.80,"Q^{2}_{e,max}<10^{5} GeV^{2};  Q^{2}_{p,max}<10^{5} GeV^{2}");
+TLatex *t3a = new TLatex(0.40,0.80,"Q^{2}_{e,max}<10^{3} GeV^{2};  Q^{2}_{p,max}<10^{3} GeV^{2}");
                 t3a->SetNDC();
                 t3a->SetTextFont(42);
                 t3a->SetTextSize(0.04);
@@ -561,7 +564,7 @@ histq2->GetYaxis()->SetTitleFont(22);
 //histq2->GetYaxis()->SetRangeUser(1,100);
 
 
- cout<<"Integral(q2e) ="<<histq2->Integral()<<endl;
+ cout<<"Integral(q2e) ="<<histq2->Integral() / bin_width_correction <<endl;
 
    // histq2->SetFillStyle(3001);
 //    histq2->SetFillColor(kGreen+1);
@@ -579,7 +582,7 @@ histq2->GetYaxis()->SetTitleFont(22);
  c8->SetLogy(1);
 
 
-c8->SaveAs("q2e_LHeC_100000GeV2.pdf");
+c8->SaveAs("q2e_LHeC_10GeV2.pdf");
 //c8->SaveAs("q2e_LHeC.C");
 //c8->SaveAs("q2e_LHeC.eps");
 //c8->SaveAs("q2e_LHeC.root");
