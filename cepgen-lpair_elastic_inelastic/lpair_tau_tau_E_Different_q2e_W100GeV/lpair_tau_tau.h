6dae0857a0a888fd0f1898d669c54ae7e7033c71
Hamzeh Khanpour <hamzeh.khanpour@gmail.com>
2024-06-09 14:25:07 +0200

    updates

--- cepgen-lpair_elastic_inelastic/lpair_tau_tau_E_Different_q2e_W100GeV/lpair_tau_tau.h
+++ cepgen-lpair_elastic_inelastic/lpair_tau_tau_E_Different_q2e_W100GeV/lpair_tau_tau.h
@@ -116,9 +116,9 @@ lpair_tau_tau::lpair_tau_tau(TTree *tree) : fChain(0)
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/home/hamzeh-khanpour/hepmc3-build/event_tautau_E_1000_1000_W100GeV.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/home/hamzeh-khanpour/hepmc3-build/event_tautau_E_100000_100000_W100GeV.root");
       if (!f || !f->IsOpen()) {
-         f = new TFile("/home/hamzeh-khanpour/hepmc3-build/event_tautau_E_1000_1000_W100GeV.root");
+         f = new TFile("/home/hamzeh-khanpour/hepmc3-build/event_tautau_E_100000_100000_W100GeV.root");
       }
       f->GetObject("hepmc3_tree",tree);
 
