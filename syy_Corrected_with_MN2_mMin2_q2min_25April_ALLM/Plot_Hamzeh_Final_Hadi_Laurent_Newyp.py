901c10cfdd2de40153212fac5a498b27222032bc
Hamzeh Khanpour <hamzeh.khanpour@gmail.com>
2024-12-19 12:11:51 +0100

    updates
    updates

---
+++ syy_Corrected_with_MN2_mMin2_q2min_25April_ALLM/Plot_Hamzeh_Final_Hadi_Laurent_Newyp.py
@@ -0,0 +1,92 @@
+
+# Final Version -- December 2024 -- Hamzeh Khanpour
+
+# ================================================================================
+
+import mplhep as hep
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+hep.style.use("CMS")
+#plt.style.use(hep.style.ROOT)
+
+'''plt.rcParams["axes.linewidth"] = 1.8
+plt.rcParams["xtick.major.width"] = 1.8
+plt.rcParams["xtick.minor.width"] = 1.8
+plt.rcParams["ytick.major.width"] = 1.8
+plt.rcParams["ytick.minor.width"] = 1.8
+
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+
+plt.rcParams["xtick.labelsize"] = 15
+plt.rcParams["ytick.labelsize"] = 15
+
+plt.rcParams["legend.fontsize"] = 15
+
+plt.rcParams['legend.title_fontsize'] = 'x-large' '''
+
+
+
+sys.path.append('./values')
+# from syy_1_3_3_0804 import *
+# from syy_1_3_4_0805 import *
+# from syy_1_4_4_0907 import *
+from wgrid_10_100000_10_elastic_tagged_Newyp import *
+
+fig, ax = plt.subplots(figsize = (8, 8))
+plt.subplots_adjust(left=0.15, right=0.95, bottom=0.12, top=0.95)
+ax.set_xlim(10.0, 1000.0)
+ax.set_ylim(1.e-7, 1.0)
+
+inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}$ GeV$^2$)').format(inel[2])
+title_label = ('$Q^2_e<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$').format(10,np.log10(inel[1]))
+plt.loglog(wvalues[3][:303], elas1[3][:303], linestyle = 'solid',  linewidth=3, label = 'tagged elastic (LHeC@1.2TeV)')
+plt.loglog(wvalues[3][:303], elas2[3][:303], linestyle = 'dashdot',  linewidth=3, label = 'tagged elastic (LHeC@0.75TeV)')
+plt.loglog(wvalues[3][:303], inel[3][:303], linestyle = 'dotted', linewidth=3, label = inel_label)
+#plt.grid()
+
+
+
+# from syy_2_3_3_0804 import *
+# from syy_2_3_4_0805 import *
+# from syy_2_4_4_0907 import *
+from wgrid_100_100000_100000_elastic_tagged_Newyp import *
+
+inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$)').format(10,np.log10(inel[2]))
+plt.loglog(wvalues[3][:303], inel[3][:303], linestyle = 'dashed', linewidth=3, label = inel_label)
+plt.legend(title = title_label)
+
+
+
+
+# Save the output values in a text file
+output_data = np.column_stack((wvalues[3][:303], elas1[3][:303], inel[3][:303]))
+header = 'W_Value Elastic Inelastic'
+np.savetxt('output_values_Syy.txt', output_data, header=header, fmt='%0.8e', delimiter='\t')
+
+
+
+
+#font1 = {'family':'serif','color':'black','size':24}
+#font2 = {'family':'serif','color':'black','size':24}
+
+
+
+plt.xlabel("W (GeV)")
+plt.ylabel("S$_{\gamma \gamma}$ (GeV$^{-1}$)")
+
+
+
+
+plt.savefig("syy_with_MN2_mMin2_q2min_Final_25April_Modified_Newyp.pdf")
+plt.savefig("syy_with_MN2_mMin2_q2min_Final_25April_Modified_Newyp.jpg")
+
+plt.show()
+
+
+
+# ================================================================================
+
+
---
+++ yy_Corrected_with_MN2_mMin2_q2min_25April_ALLM/Plot_Hamzeh_Final_Hadi_Laurent_Newyp.py
@@ -0,0 +1,130 @@
+
+# Final Version -- December 2024 -- Hamzeh Khanpour
+
+# ================================================================================
+
+
+import mplhep as hep
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+hep.style.use("CMS")
+#plt.style.use(hep.style.ROOT)
+
+
+'''plt.rcParams["axes.linewidth"] = 1.8
+plt.rcParams["xtick.major.width"] = 1.8
+plt.rcParams["xtick.minor.width"] = 1.8
+plt.rcParams["ytick.major.width"] = 1.8
+plt.rcParams["ytick.minor.width"] = 1.8
+
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+
+plt.rcParams["xtick.labelsize"] = 15
+plt.rcParams["ytick.labelsize"] = 15
+
+plt.rcParams["legend.fontsize"] = 15
+
+plt.rcParams['legend.title_fontsize'] = 'x-large' '''
+
+
+def trap_integ(wv, fluxv):
+    wmin = np.zeros(len(wv) - 1)
+    integ = np.zeros(len(wv) - 1)
+    for i in range(len(wv) - 2, -1, -1):
+        wvwid = wv[i + 1] - wv[i]
+        traparea = wvwid * 0.5 * (fluxv[i] + fluxv[i + 1])
+        wmin[i] = wv[i]
+        if i == len(wv) - 2:
+            integ[i] = traparea
+        else:
+            integ[i] = integ[i + 1] + traparea
+
+    return wmin, integ
+
+
+sys.path.append('./values')
+
+
+
+
+from wgrid_10_100000_10_elastic_tagged_Newyp import *
+
+wv = np.array(wvalues[3])
+ie = np.array(inel[3])
+el1 = np.array(elas1[3])
+el2 = np.array(elas2[3])
+
+wv1, int_inel = trap_integ(wv, ie)
+wv2, int_el1 = trap_integ(wv, el1)
+wv2, int_el2 = trap_integ(wv, el2)
+
+fig, ax = plt.subplots(figsize = (8, 8))
+plt.subplots_adjust(left=0.15, right=0.95, bottom=0.12, top=0.95)
+ax.set_xlim(10.0, 1000.0)
+ax.set_ylim(1.e-5, 1.0e1)
+
+
+
+
+inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}$ GeV$^2$)').format(inel[2])
+title_label = ('$Q^2_e<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$').format(10,np.log10(inel[1]))
+
+plt.loglog(wv2[:303], int_el1[:303], linestyle = 'solid',  linewidth=3,  label = 'tagged elastic (LHeC@1.2TeV)')
+plt.loglog(wv2[:303], int_el2[:303], linestyle = 'dashdot',  linewidth=3,  label = 'tagged elastic (LHeC@0.75TeV)')
+
+plt.loglog(wv1[:303], int_inel[:303], linestyle = 'dotted',  linewidth=3, label = inel_label)
+#plt.grid()
+
+
+
+
+
+
+from wgrid_100_100000_100000_elastic_tagged_Newyp import *
+
+wv = np.array(wvalues[3])
+ie = np.array(inel[3])
+wv1, int_inel = trap_integ(wv, ie)
+
+inel_label = ('$M_N<$ ${{{:g}}}$ GeV').format(inel[0]) + (' ($Q^2_p<$ ${{{:g}}}^{{{:g}}}$ GeV$^2$)').format(10,np.log10(inel[2]))
+plt.loglog(wv2[:303], int_inel[:303], linestyle = 'dashed',  linewidth=3, label = inel_label)
+plt.legend(title = title_label)
+
+
+
+
+
+# Save the output values in a text file
+output_data = np.column_stack((wv2[:303], int_el1[:303], int_inel[:303]))
+header = 'W_Value Elastic Inelastic'
+np.savetxt('output_values_ntegrated_Syy.txt', output_data, header=header, fmt='%0.8e', delimiter='\t')
+
+
+
+
+
+
+#font1 = {'family':'serif','color':'black','size':24}
+#font2 = {'family':'serif','color':'black','size':24}
+
+plt.xlabel("W$_0$ [GeV]")
+plt.ylabel("Integrated S$_{\gamma \gamma}$ (W > W$_0$)")
+
+
+
+
+plt.savefig("yy_int_with_MN2_mMin2_q2min_Final_25April_Modified_Newyp.pdf")
+plt.savefig("yy_int_with_MN2_mMin2_q2min_Final_25April_Modified_Newyp.jpg")
+
+
+
+
+plt.show()
+
+
+
+#===========================================================================
+
